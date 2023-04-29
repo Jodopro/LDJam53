db44756b3bc082ba620ff4d5f401d8b1cb96c216
H.C. Jansen <chris9898@live.nl>
2023-04-29 21:16:01 +0200

    World API

---
+++ core/src/com/lipsum/game/utils/Twople.java
@@ -0,0 +1,11 @@
+package com.lipsum.game.utils;
+
+public class Twople<First_T, Second_T> {
+    public First_T first;
+    public Second_T second;
+
+    public Twople(First_T first, Second_T second) {
+        this.first = first;
+        this.second = second;
+    }
+}
--- core/src/com/lipsum/game/world/World.java
+++ core/src/com/lipsum/game/world/World.java
@@ -2,45 +2,46 @@ package com.lipsum.game.world;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.lipsum.game.utils.Twople;
 import com.lipsum.game.world.tile.BackgroundTile;
 import com.lipsum.game.world.tile.Tile;
 
 import java.util.HashMap;
+import java.util.function.BiFunction;
 
 public class World extends Actor {
-    public final int CHUNK_SIZE;
+    public final int CHUNK_WIDTH_IN_TILES;
 
     private HashMap<Coordinate, Chunk> chunks;
     private final OrthographicCamera camera;
     private final Texture cameraTexture = new Texture("camera.png");
 
     // ingore these variables :)
-    private Coordinate estimatedCoord;
-    private Coordinate[] localArea;
+    private Coordinate currentChunkCoord;
+    private Coordinate[] localChunkArea;
 
-    public World(int chunkSize, OrthographicCamera camera) {
+    public World(int chunkSideLengthInTiles, OrthographicCamera camera) {
         this.camera = camera;
         this.camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
         this.camera.zoom = 10f;
         this.camera.update();
 
-        this.CHUNK_SIZE = chunkSize;
+        this.CHUNK_WIDTH_IN_TILES = chunkSideLengthInTiles;
         this.chunks = new HashMap<>();
         makeBackgroundChunk(0, 0);
     }
 
     private Chunk makeBackgroundChunk(int x, int y) {
         var coord = new Coordinate(x, y);
-        var newChunk = new Chunk(coord, new BackgroundTile[CHUNK_SIZE][CHUNK_SIZE]);
-        for (int rowI = 0; rowI < CHUNK_SIZE; rowI++) {
-            for (int colI = 0; colI < CHUNK_SIZE; colI++) {
-                newChunk.tiles[rowI][colI] = new BackgroundTile(colI, rowI, this);
+        var newChunk = new Chunk(coord, new BackgroundTile[CHUNK_WIDTH_IN_TILES][CHUNK_WIDTH_IN_TILES]);
+        for (int rowI = 0; rowI < CHUNK_WIDTH_IN_TILES; rowI++) {
+            for (int colI = 0; colI < CHUNK_WIDTH_IN_TILES; colI++) {
+                newChunk.tiles[rowI][colI] = new BackgroundTile(colI, rowI, newChunk);
             }
         }
         chunks.put(coord, newChunk);
@@ -49,24 +50,24 @@ public class World extends Actor {
     }
 
     public void step() {
-        var chunkSize = Tile.WIDTH * CHUNK_SIZE;
-        estimatedCoord = new Coordinate((int)Math.floor(camera.position.x / chunkSize), (int)Math.floor(camera.position.y / chunkSize));
+        var chunkSize = Tile.WIDTH * CHUNK_WIDTH_IN_TILES;
+        currentChunkCoord = new Coordinate((int)Math.floor(camera.position.x / chunkSize), (int)Math.floor(camera.position.y / chunkSize));
 
         handleInput();
         camera.update();
-        localArea = new Coordinate[]{
-            new Coordinate(estimatedCoord.x() + 1, estimatedCoord.y() + 1),
-            new Coordinate(estimatedCoord.x(), estimatedCoord.y() + 1),
-            new Coordinate(estimatedCoord.x() - 1, estimatedCoord.y() + 1),
-            new Coordinate(estimatedCoord.x() - 1, estimatedCoord.y()),
-            new Coordinate(estimatedCoord.x(), estimatedCoord.y()),
-            new Coordinate(estimatedCoord.x() + 1, estimatedCoord.y()),
-            new Coordinate(estimatedCoord.x() -1, estimatedCoord.y() - 1),
-            new Coordinate(estimatedCoord.x(), estimatedCoord.y() - 1),
-            new Coordinate(estimatedCoord.x() + 1, estimatedCoord.y() - 1),
+        localChunkArea = new Coordinate[]{
+            new Coordinate(currentChunkCoord.x() + 1, currentChunkCoord.y() + 1),
+            new Coordinate(currentChunkCoord.x(), currentChunkCoord.y() + 1),
+            new Coordinate(currentChunkCoord.x() - 1, currentChunkCoord.y() + 1),
+            new Coordinate(currentChunkCoord.x() - 1, currentChunkCoord.y()),
+            new Coordinate(currentChunkCoord.x(), currentChunkCoord.y()),
+            new Coordinate(currentChunkCoord.x() + 1, currentChunkCoord.y()),
+            new Coordinate(currentChunkCoord.x() -1, currentChunkCoord.y() - 1),
+            new Coordinate(currentChunkCoord.x(), currentChunkCoord.y() - 1),
+            new Coordinate(currentChunkCoord.x() + 1, currentChunkCoord.y() - 1),
         };
 
-        for (var n : localArea) {
+        for (var n : localChunkArea) {
             if (chunks.get(n) == null) {
                 chunks.put(n, makeBackgroundChunk(n.x(), n.y()));
             }
@@ -77,13 +78,43 @@ public class World extends Actor {
     public void draw(Batch batch, float parentAlpha) {
         batch.setProjectionMatrix(camera.combined);
 
-        for (var n : localArea) {
+        for (var n : localChunkArea) {
             if (chunks.get(n) != null) {
                 chunks.get(n).draw(batch);
             }
         }
 
         batch.draw(cameraTexture, camera.position.x - 16 , camera.position.y - 16, 32, 32);
+    }
+
+    public Chunk chunkAt(float absoluteX, float absoluteY) {
+        return chunks.get(currentChunkCoord);
+    }
+
+    public Tile tileAt(float absoluteX, float absoluteY) {
+        var chunkSize = Tile.WIDTH * CHUNK_WIDTH_IN_TILES;
+        BiFunction<Float, Float, Float> difference = (a, b) -> {
+            var absA =  Math.abs(a);
+            var absB = Math.abs(b);
+            return absA > absB ? absA - absB : absB - absA;
+        };
+
+        int row = (int)Math.floor(difference.apply(absoluteX, (float)(currentChunkCoord.x() * chunkSize)) / Tile.WIDTH);
+        int col = (int)Math.floor(difference.apply(absoluteY, (float)(currentChunkCoord.y() * chunkSize)) / Tile.HEIGHT);
+        return chunks.get(currentChunkCoord).tiles[row][col];
+    }
+
+    public Coordinate gridCoordinateOf(float absoluteX, float absoluteY) {
+        int x = (int)Math.floor(absoluteX / Tile.WIDTH);
+        int y = (int)Math.floor(absoluteY / Tile.HEIGHT);
+        return new Coordinate(x, y);
+    }
+
+    public Twople<Float, Float> absolutePositionOf(Tile tile) {
+        var chunkSize = Tile.WIDTH * CHUNK_WIDTH_IN_TILES;
+        float x = tile.chunk.coordinate.x() * chunkSize + tile.x * Tile.WIDTH;
+        float y = tile.chunk.coordinate.y() * chunkSize + tile.y * Tile.HEIGHT;
+        return new Twople<>(x, y);
     }
 
     public void dispose() {
