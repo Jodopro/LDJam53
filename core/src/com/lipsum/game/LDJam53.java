981beb4ff0796c30e7ceff011e51e5ccea83079c
gerben <gerry2211@gmail.com>
2023-04-29 11:28:46 +0200

    Entity, Factory, Event setup

--- core/src/com/lipsum/game/LDJam53.java
+++ core/src/com/lipsum/game/LDJam53.java
@@ -4,10 +4,18 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.lipsum.game.event.EventQueue;
+import com.lipsum.game.factory.factories.EntityFactory;
 
 public class LDJam53 extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	static {
+		// Init all factories here, since static blocks are only executed when the class is used.
+		// Missing factories here will potentially make them invisible to super-factories
+		EntityFactory.getInstance();
+	}
 	
 	@Override
 	public void create () {
@@ -17,6 +25,8 @@ public class LDJam53 extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		EventQueue.getInstance().handleAll();
+
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
