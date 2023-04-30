3b88f22733b16b0c14e97339b1687d337dbe47e5
gerben <gerry2211@gmail.com>
2023-04-30 16:05:10 +0200

    Delete and rotate buttons

--- core/src/com/lipsum/game/TextureStore.java
+++ core/src/com/lipsum/game/TextureStore.java
@@ -29,6 +29,9 @@ public class TextureStore {
     public static final Texture PACKET = new Texture(Gdx.files.internal("textures/packet.png"));
 
     public static final Texture BUILD_BUTTON = new Texture(Gdx.files.internal("textures/build_button.png"));
+    public static final Texture ROTATE = new Texture(Gdx.files.internal("textures/build_button.png"));
+    public static final Texture DELETE = new Texture(Gdx.files.internal("textures/build_button.png"));
+
 
 
 
